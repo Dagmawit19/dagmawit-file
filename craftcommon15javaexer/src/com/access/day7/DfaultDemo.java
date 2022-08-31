a1afa109e793e4771788aade1f35f3f488f2469b
Dagmawit19 <dagmawit_belete@yahoo.com>
2022-08-30 23:06:42 -0400

    some message

---
+++ craftcommon15javaexer/src/com/access/day7/DfaultDemo.java
@@ -0,0 +1,22 @@
+package com.access.day7;
+
+class DefaultTest{
+	
+	String name = "JOHN";
+	
+	void show() {
+		System.out.println(name);
+	}	
+}		
+	
+
+   public class DfaultDemo {
+	   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+         DefaultTest d1 = new DefaultTest();
+         d1.show();
+	}
+
+   } 
