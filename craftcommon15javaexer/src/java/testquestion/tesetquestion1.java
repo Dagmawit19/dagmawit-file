99c8e47ee18cf1f704f35d3bc1e36150593fa4fa
Dagmawit19 <dagmawit_belete@yahoo.com>
2022-08-30 23:45:08 -0400

    java test

---
+++ craftcommon15javaexer/src/java/testquestion/tesetquestion1.java
@@ -0,0 +1,13 @@
+package java.testquestion;
+
+public class tesetquestion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i+=2) {
+			System.out.print(i);
+		}
+
+	}
+
+}
